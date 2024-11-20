6795efe1b4ad7d8f6959f976c6c589715c9a4c70
parentn <nicolas2parent@gmail.com>
2024-11-20 15:06:35 +0100

    [DATA_EXTRACTOR] pdf_cutter and functional table extractor.

--- scripts/main.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-print(f"Hello world, arguments: {list(sys.argv)}")
-
-import psycopg2
---
+++ src/__main__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+
+import psycopg2
+
+if __name__=="__main__":
+    print(f"Hello world, arguments: {list(sys.argv)}")
